2521b00f8d585b7404c6910a2889bd0a23efb266
Kavan Patel <kavanpatel2403@gmail.com>
2020-04-24 09:39:06 +0530

    changes made

--- config/keys-prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://kavan:Kavan1234@myfirstapp-hanzp.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID: '',
-    GoogleClientSecret: ''
-}   
